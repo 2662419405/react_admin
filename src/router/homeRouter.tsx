47b697e90bd722899aaf95cd0bd01a3569e9baf8
孙航 <2662419405@qq.com>
2020-05-05 09:43:18 +0800

    改为解构

--- src/router/homeRouter.tsx
+++ src/router/homeRouter.tsx
@@ -1,22 +1,34 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useCallback } from "react";
 import { Route, Switch } from "react-router-dom";
 import { Loading, ErrorBoundary } from "src/components";
 
-const Home = React.lazy(() => import("src/views/Home/home"));
-const About = React.lazy(() => import("src/views/Home/about"));
-const NotFound = React.lazy(() => import("src/views/Home/NotFound"));
+const [Home, About, NotFound] = [
+  () => import(`src/views/Home/home`),
+  () => import(`src/views/Home/about`),
+  () => import(`src/views/Home/NotFound`),
+].map((item) => {
+  return React.lazy(item);
+});
 
 interface Iprops {
   location: Location;
 }
 
 const Routers: React.FC<Iprops> = (props) => {
+  /**
+   * 切换路由触发
+   * 模拟vue的路由守卫
+   */
+  const onEnter = useCallback((Component) => {
+    return <Component {...props} />;
+  }, []);
+
   return (
     <ErrorBoundary location={props.location}>
       <Suspense fallback={<Loading />}>
         <Switch>
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/home/about" component={About} />
+          <Route exact path="/home" render={() => onEnter(Home)} />
+          <Route exact path="/home/about" render={() => onEnter(About)} />
           <Route component={NotFound} />
         </Switch>
       </Suspense>
