e8fe2408639cebc2d848066d945bb40a50b32317
孙航 <2662419405@qq.com>
2020-06-01 00:37:31 +0800

    :100: 完善

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,19 @@
-const { override, fixBabelImports, addLessLoader } = require("customize-cra");
+const { override, fixBabelImports, addLessLoader } = require('customize-cra')
+const rewiredMap = () => config => {
+  config.devtool =
+    config.mode === 'development' ? 'cheap-module-source-map' : false
+  return config
+}
 
 module.exports = override(
-  fixBabelImports("import", {
-    libraryName: "antd",
-    libraryDirectory: "es",
-    style: true, // change importing css to less
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
   }),
   addLessLoader({
     javascriptEnabled: true,
-    modifyVars: { "@primary-color": "#1890ff" },
-  })
-);
+    modifyVars: { '@primary-color': '#1DA57A' },
+  }),
+  rewiredMap()
+)
