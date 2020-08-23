bdec7086b3f145ba01f6fdc3de7a95fe9437dfcc
孙航 <2662419405@qq.com>
2020-08-24 00:21:18 +0800

    修复代理

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,8 +3,9 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: "https://work.xiejiahe.com/", //搜索跨域
+      target: "https://work.xiejiahe.com", //搜索跨域
       changeOrigin: true,
     })
   );
 };
+ vb
