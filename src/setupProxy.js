21106303a154f91a2dd4ef3bf6a3ec0810ace112
孙航 <2662419405@qq.com>
2020-08-23 23:49:50 +0800

    bugFix: 修复代理问题

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api", {
-      target: "https://tomato-work.xiejiahe.com/", //搜索跨域
+      target: "https://work.xiejiahe.com/", //搜索跨域
       changeOrigin: true,
     })
   );
